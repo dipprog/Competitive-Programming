e31c0121b10c9aa43ca1f2284ca6f45db043d6fb
dipprog <dipkumardasdh@gmail.com>
2019-01-25 20:11:11 +0530

    Brute force solution in  O(n^2)

---
+++ Arrays_Questions/Majority Element/solutionBasic.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+using namespace std;
+void findMajority(int arr[], int n){
+  int maxCount = 0;
+  int index = -1;
+  for(int i=0; i<n; i++){
+    int count = 0;
+    for(int j=0; j<n; j++){
+      if(arr[i] == arr[j])
+        count++;
+    }
+    if(count > maxCount){
+      maxCount = count;
+      index = i;
+    }
+  }
+  if(maxCount > n/2)
+    cout<<arr[index]<<endl;
+  else 
+    cout<<"No Majority Element Exist"<<endl;
+}
+int main() {
+  int arr[50];
+  int n;
+  cin>>n;
+  for(int i = 0; i<n; i++){
+    cin>>arr[i];
+  }
+  findMajority(arr, n);
+  return 0;
+}
